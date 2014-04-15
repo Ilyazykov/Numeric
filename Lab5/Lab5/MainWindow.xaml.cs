06ed07bd3310d55c17252886f50e27a3c78dfe5c
Ilya Zykov <ilya.zykov@icloud.com>
2014-04-15 19:32:42 +0400

    Example graph of the function

--- Lab5/Lab5/MainWindow.xaml.cs
+++ Lab5/Lab5/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,9 +22,47 @@ namespace Lab5
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<ChartPoint> ChartData;
+        private Random Rand;
+        private DateTime CurTime;
+
         public MainWindow()
         {
             InitializeComponent();
+            CurTime = DateTime.Now;
+            Rand = new Random();
+            ChartData = new ObservableCollection<ChartPoint>
+            {
+                new ChartPoint{ Value = 10, Time = CurTime + TimeSpan.FromSeconds(10) },
+                new ChartPoint{ Value = 20, Time = CurTime + TimeSpan.FromSeconds(20) },
+                new ChartPoint{ Value = 30, Time = CurTime + TimeSpan.FromSeconds(30) },
+                new ChartPoint{ Value = 10, Time = CurTime + TimeSpan.FromSeconds(40) },
+                new ChartPoint{ Value = 40, Time = CurTime + TimeSpan.FromSeconds(50) },
+            };
+            ChartOne.ItemsSource = ChartData;
         }
+
+        private void btnAddChart_Click(object sender, RoutedEventArgs e)
+        {
+            LineSeries NewChart = new LineSeries();
+            NewChart.ItemsSource = new ObservableCollection<ChartPoint>
+            {
+                new ChartPoint{ Value = Rand.Next(5,45), Time = CurTime + TimeSpan.FromSeconds(10) },
+                new ChartPoint{ Value = Rand.Next(5,45), Time = CurTime + TimeSpan.FromSeconds(20) },
+                new ChartPoint{ Value = Rand.Next(5,45), Time = CurTime + TimeSpan.FromSeconds(30) },
+                new ChartPoint{ Value = Rand.Next(5,45), Time = CurTime + TimeSpan.FromSeconds(40) },
+                new ChartPoint{ Value = Rand.Next(5,45), Time = CurTime + TimeSpan.FromSeconds(50) },
+            };
+            NewChart.DependentValuePath = "Value";
+            NewChart.IndependentValuePath = "Time";
+            NewChart.Title = "New Chart!";
+            Charts.Series.Add(NewChart);
+        }
+    }
+
+    public class ChartPoint
+    {
+        public int Value { get; set; }
+        public DateTime Time { get; set; }
     }
 }
