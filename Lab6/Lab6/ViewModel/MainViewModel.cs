170c393b3523b2ceb29f373b5af5044b079f9dd7
Ilya Zykov <ilya.zykov@icloud.com>
2014-04-21 07:36:23 +0400

    Chart added

--- Lab6/Lab6/ViewModel/MainViewModel.cs
+++ Lab6/Lab6/ViewModel/MainViewModel.cs
@@ -1,34 +1,20 @@
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
+using Lab6.Model;
 
 namespace Lab6.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        public ObservableCollection<ChartPoint> ChartData { get; set; } 
+
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            ChartData = new ObservableCollection<ChartPoint>();
+
+            ChartData.Add(new ChartPoint(1,1));
+            ChartData.Add(new ChartPoint(2,3));
+            ChartData.Add(new ChartPoint(3,2));
         }
     }
 }
