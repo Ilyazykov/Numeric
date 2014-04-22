868224682c340a88a287b12e7003a92c60a666a3
Ilya Zykov <ilya.zykov@icloud.com>
2014-04-22 07:17:11 +0400

    FirstTask

--- Lab6/Lab6/ViewModel/FirstTaskViewModel.cs
+++ Lab6/Lab6/ViewModel/FirstTaskViewModel.cs
@@ -1,40 +1,127 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using Lab6.Model;
+using Lab6.Model.DifferentialEquation;
 
 namespace Lab6.ViewModel
 {
     public class FirstTaskViewModel : ViewModelBase
     {
-        public ObservableCollection<ChartPoint> ChartData { get; set; }
-        public ObservableCollection<MajorTableData> ResultTable { get; set; } 
+        public ObservableCollection<ChartPoint> NumericalChartData { get; set; }
+        public ObservableCollection<MajorTableData> ResultTable { get; set; }
+
+        private double _y0;
+        public double Y0
+        {
+            get { return _y0; }
+            set
+            {
+                _y0 = value;
+                _equation.Y0 = value;
+            }
+        }
+
+        public int MaximumNumberOfIteration { get; set; }
+        public double EpsilonUp { get; set; }
+
+        private bool _isFixedStep;
+        public bool IsFixedStep
+        {
+            get { return _isFixedStep; }
+            set
+            {
+                _isFixedStep = value;
+                RaisePropertyChanged("IsFixedStep");
+            }
+        }
+
+        public double StepSize { get; set; }
+
+        public RelayCommand СalculationCommand { get; set; }
+        private void СalculationCommandExecutor()
+        {
+            GetTable();
+            GetCharts();
+        }
+
+        private DiferentialEquation _equation;
 
         public FirstTaskViewModel()
         {
-            GetFakeChartData();
-            GetFakeTable();
+            _equation = new FirstTask(1);
+
+            Y0 = 1;
+            MaximumNumberOfIteration = 10;
+            IsFixedStep = false;
+            StepSize = 0.1;
+            EpsilonUp = 0.001;
+
+            СalculationCommandExecutor();
+
+            СalculationCommand = new RelayCommand(СalculationCommandExecutor);
         }
 
-        private void GetFakeChartData()
-        {
-            ChartData = new ObservableCollection<ChartPoint>();
-
-            ChartData.Add(new ChartPoint(1, 1));
-            ChartData.Add(new ChartPoint(2, 4));
-            ChartData.Add(new ChartPoint(3, 1));
-            ChartData.Add(new ChartPoint(4, 4));
-
-            RaisePropertyChanged("AnaliticalChartData");
-        }
-
-        private void GetFakeTable()
+        private void GetTable()
         {
             ResultTable = new ObservableCollection<MajorTableData>();
 
-            ResultTable.Add(new MajorTableData(1, 1, 1, 1, 1, 1, 1, 1));
-            ResultTable.Add(new MajorTableData(2, 2, 2, 2, 2, 2, 2, 2));
+            var dx = StepSize;
+            double xPrev = 0;
+            double yPrev = Y0;
 
+            int c1 = 0;
+            int c2 = 0;
+            ResultTable.Add(new MajorTableData(0, Y0, Y0, 0, 0, dx, 0, 0));
+
+            for (int i = 1; i < MaximumNumberOfIteration; i++)
+            {
+                double x = xPrev + dx;
+                double y = _equation.GetNumericalValue(x, xPrev, yPrev);
+
+                double x2 = xPrev + dx / 2;
+                double y2 = _equation.GetNumericalValue(x2, xPrev, yPrev);
+                y2 = _equation.GetNumericalValue(x, x2, y2);
+
+                double estimationOfLocalError = Math.Abs(y2 - y) / 15;
+
+                if (!IsFixedStep)
+                {
+                    if (estimationOfLocalError < EpsilonUp / 32)
+                    {
+                        dx *= 2;
+                        c2++;
+                    }
+                    else if (estimationOfLocalError > EpsilonUp)
+                    {
+                        y = y2; //TODO нужен ли?
+                        dx /= 2;
+                        c1++;
+                    }
+                }
+
+                ResultTable.Add(new MajorTableData(x, y, y2, Math.Abs(y - y2), estimationOfLocalError, dx, c1, c2));
+
+                xPrev = x;
+                yPrev = y;
+
+            }
             RaisePropertyChanged("ResultTable");
+        }
+
+        private void GetCharts()
+        {
+            NumericalChartData = new ObservableCollection<ChartPoint>();
+
+            for (int i = 0; i < MaximumNumberOfIteration; i++)
+            {
+                if (double.IsInfinity(ResultTable[i].V)) break;
+
+                NumericalChartData.Add(new ChartPoint(ResultTable[i].X, ResultTable[i].V));
+            }
+
+            RaisePropertyChanged("NumericalChartData");
         }
     }
 }
--- Lab6/Lab6/ViewModel/TestTaskViewModel.cs
+++ Lab6/Lab6/ViewModel/TestTaskViewModel.cs
@@ -15,7 +15,6 @@ namespace Lab6.ViewModel
         public ObservableCollection<ChartPoint> NumericalChartData { get; set; }
         public ObservableCollection<TestTableData> ResultTable { get; set; }
         
-        
         private double _y0;
         public double Y0
         {
@@ -47,7 +46,7 @@ namespace Lab6.ViewModel
         private void СalculationCommandExecutor()
         {
             GetTable();
-            GetFakeTable();
+            GetCharts();
         }
 
         private DiferentialEquation _equation;
@@ -58,46 +57,13 @@ namespace Lab6.ViewModel
 
             Y0 = 1;
             MaximumNumberOfIteration = 10;
-            IsFixedStep = true;
+            IsFixedStep = false;
             StepSize = 1;
             EpsilonUp = 0.001;
-            
-            _equation  = new TestTask(Y0);
 
             СalculationCommandExecutor();
 
             СalculationCommand = new RelayCommand(СalculationCommandExecutor);
-        }
-
-        private void GetChartDataWithFixedStep()
-        {
-            ResultTable = new ObservableCollection<TestTableData>();
-
-            var dx = StepSize;
-            double xPrev = 0;
-            double yPrev = Y0;
-
-            ResultTable.Add(new TestTableData(0, Y0, Y0, 0, 0, dx, 0, 0, Y0, 0));
-
-            for (int i = 0; i < MaximumNumberOfIteration; i++)
-            {
-                double x = xPrev + dx;
-                double y = _equation.GetNumericalValue(x, xPrev, yPrev);
-
-                double x2 = xPrev + dx / 2;
-                double y2 = _equation.GetNumericalValue(x2, xPrev, yPrev);
-                y2 = _equation.GetNumericalValue(x, x2, y2);
-
-                double estimationOfLocalError = Math.Abs(y2 - y) / 15;
-
-                double u = _equation.GetAnaliticalValue(x);
-
-                ResultTable.Add(new TestTableData(x, y, y2, Math.Abs(y - y2), estimationOfLocalError, dx, 0, 0, u, Math.Abs(u - y)));
-
-                xPrev = x;
-                yPrev = y;
-            }
-            RaisePropertyChanged("ResultTable");
         }
 
         private void GetTable()
@@ -147,7 +113,7 @@ namespace Lab6.ViewModel
             RaisePropertyChanged("ResultTable");
         }
 
-        private void GetFakeTable()
+        private void GetCharts()
         {
             AnaliticalChartData = new ObservableCollection<ChartPoint>();
             NumericalChartData = new ObservableCollection<ChartPoint>();
