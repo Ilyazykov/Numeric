981ba639cbe79df0adeb813cbff2da1ecce9ec00
Ilya Zykov <ilya.zykov@icloud.com>
2014-04-25 13:39:07 +0400

    Template of minimal residual method.

--- Lab4/dirichletProblem/getterOfValues/differentialEquation.cs
+++ Lab4/dirichletProblem/getterOfValues/differentialEquation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using dirichletProblem.Functions;
@@ -114,6 +115,89 @@ namespace dirichletProblem.getterOfValues
 
             Table res = new Table(v, x, y, S, eps_max);
             return res;
+        }
+
+        private Table minimalResidualMethod(BorderValues borderValues, int numberOfIteration, double additional = 1)
+        {
+            //bool who_is_old = false;
+            //numberOfIteration = 0;
+            //double Es;
+            //do
+            //{
+            //    Es = Step();
+            //    numberOfIteration++;
+            //} while ((numberOfIteration<additional)&&(Es>Emin));
+
+            Table res = new Table();
+            //if (who_is_old)
+            //{
+            //    res = res_1;
+            //}
+            //else
+            //{
+            //    res = res_0;
+            //}
+            return res;
+        }
+
+        public double Step()
+        {
+            //double[,] V_new;
+            //double[,] V_old;
+
+            //if (who_is_old) { V_new = V_0; V_old = V_1; }
+            //else { V_new = V_1; V_old = V_0; }
+            //who_is_old = !who_is_old;
+            double Es = 0;
+            //double tay1 = 0;
+            //double tay2 = 0;
+            //double tay_s;
+
+            //for (int j = 1; j < m; j++)
+            //    for (int i = n2 + 1; i < n; i++)
+            //    {
+            //        r[i, j] = F[i, j] + h2 * (V_old[i - 1, j] + V_old[i + 1, j]) + k2 * (V_old[i, j - 1] + V_old[i, j + 1]) + A * V_old[i, j];
+            //    }//обход правого прямоугольника 
+
+            //for (int j = m2 + 1; j < m; j++)
+            //    for (int i = 1; i <= n2; i++)
+            //    {
+            //        r[i, j] = F[i, j] + h2 * (V_old[i - 1, j] + V_old[i + 1, j]) + k2 * (V_old[i, j - 1] + V_old[i, j + 1]) + A * V_old[i, j];
+            //    }//обход левого верхнего квадрата 
+
+
+            //for (int j = 1; j < m; j++)
+            //    for (int i = n2 + 1; i < n; i++)
+            //    {
+            //        Ar[i, j] = h2 * (r[i - 1, j] + r[i + 1, j]) + k2 * (r[i, j - 1] + r[i, j + 1]) + A * r[i, j];
+            //        tay1 = tay1 + Ar[i, j] * r[i, j];
+            //        tay2 = tay2 + Ar[i, j] * Ar[i, j];
+            //    }
+
+            //for (int j = m2 + 1; j < m; j++)
+            //    for (int i = 1; i <= n2; i++)
+            //    {
+            //        Ar[i, j] = h2 * (r[i - 1, j] + r[i + 1, j]) + k2 * (r[i, j - 1] + r[i, j + 1]) + A * r[i, j];
+            //        tay1 = tay1 + Ar[i, j] * r[i, j];
+            //        tay2 = tay2 + Ar[i, j] * Ar[i, j];
+            //    }
+            //tay_s = tay1 / tay2;
+
+            //for (int j = 1; j < m; j++)
+            //    for (int i = n2 + 1; i < n; i++)
+            //    {
+            //        V_new[i, j] = V_old[i, j] - tay_s * r[i, j];
+            //        if (Math.Abs(V_new[i, j] - V_old[i, j]) > Es) Es = Math.Abs(V_new[i, j] - V_old[i, j]);
+            //    }
+
+            //for (int j = m2 + 1; j < m; j++)
+            //    for (int i = 1; i <= n2; i++)
+            //    {
+            //        V_new[i, j] = V_old[i, j] - tay_s * r[i, j];
+            //        if (Math.Abs(V_new[i, j] - V_old[i, j]) > Es) Es = Math.Abs(V_new[i, j] - V_old[i, j]);
+            //    }
+
+            return Es;
         }
 
         private Table seidelMethod(BorderValues borderValues, int numberOfIteration, double eps, double additional = 1)
